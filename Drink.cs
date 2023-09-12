6b6d325d0d6353a981b8e7df08268bc25eabdb71
Surgexuan1208 <jeffs941208@gmail.com>
2023-09-13 03:58:13 +0800

    drink console app

---
+++ Drink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drink
+{
+    internal class Drink
+    {
+        public string name { get; set; }
+        public string size { get; set; }
+        public int price { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+namespace drink
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            List<Drink> drinks = new List<Drink>();
+            drinks.Add(new Drink() { name = "紅茶", size = "大杯", price = 50 });
+            drinks.Add(new Drink() { name = "紅茶", size = "小杯", price = 30 });
+            drinks.Add(new Drink() { name = "綠茶", size = "大杯", price = 50 });
+            drinks.Add(new Drink() { name = "綠茶", size = "小杯", price = 30 });
+            drinks.Add(new Drink() { name = "咖啡", size = "大杯", price = 60 });
+            drinks.Add(new Drink() { name = "咖啡", size = "小杯", price = 40 });
+        }
+    }
+}
